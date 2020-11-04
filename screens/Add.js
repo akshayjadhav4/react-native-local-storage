3232026aba517827f1c09f6e029a6eedd05709d6
AKSHAY <akshayjadhav461999@gmail.com>
2020-11-04 14:06:34 +0530

    storing series in storage

--- screens/Add.js
+++ screens/Add.js
@@ -1,11 +1,75 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {Text, ScrollView, StyleSheet} from 'react-native';
+import {Container, Form, Item, Input, Button, H1} from 'native-base';
+import shortid from 'shortid';
+import AsyncStorage from '@react-native-community/async-storage';
+import Snackbar from 'react-native-snackbar';
+export default function Add({navigation}) {
+  const [name, setName] = useState('');
+  const [totalNumberOfSeason, setTotalNumberOfSeason] = useState('');
 
-export default function Add() {
+  const saveSeries = async () => {
+    try {
+      // check if input is provided
+      if (!name || !totalNumberOfSeason) {
+        return Snackbar.show({
+          text: 'Please provide both fields',
+          duration: Snackbar.LENGTH_SHORT,
+          backgroundColor: '#00b7c2',
+          textColor: '#eee',
+        });
+      }
+      // make object to add in storage
+      const seriesToAdd = {
+        id: shortid.generate(),
+        name,
+        totalNumberOfSeason,
+        isWatched: false,
+      };
+      // get already created storage
+      const storedValues = await AsyncStorage.getItem('@series_list');
+      const prevList = await JSON.parse(storedValues);
+      // check already storage exists
+      if (!prevList) {
+        const newList = [seriesToAdd];
+        await AsyncStorage.setItem('@series_list', JSON.stringify(newList));
+      } else {
+        prevList.push(seriesToAdd);
+        await AsyncStorage.setItem('@series_list', JSON.stringify(prevList));
+      }
+      // navigate to home after adding
+      navigation.navigate('Home');
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-    <View>
-      <Text>Add Screen</Text>
-    </View>
+    <Container style={styles.container}>
+      <ScrollView contentContainerStyle={{flexGrow: 1}}>
+        <H1 style={styles.heading}>Add to watch list</H1>
+        <Form>
+          <Item rounded style={styles.formItem}>
+            <Input
+              placeholder="Series Name"
+              style={{color: '#eee'}}
+              value={name}
+              onChangeText={(text) => setName(text)}
+            />
+          </Item>
+          <Item rounded style={styles.formItem}>
+            <Input
+              placeholder="Total number of seasons"
+              style={{color: '#eee'}}
+              value={totalNumberOfSeason}
+              onChangeText={(text) => setTotalNumberOfSeason(text)}
+            />
+          </Item>
+          <Button rounded block onPress={saveSeries}>
+            <Text style={{color: '#eee'}}>Add</Text>
+          </Button>
+        </Form>
+      </ScrollView>
+    </Container>
   );
 }
 
