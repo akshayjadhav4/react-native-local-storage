dd975d95e73de3dc5f3c6f5992c4b4c3eee291ac
AKSHAY <akshayjadhav461999@gmail.com>
2020-11-04 16:22:11 +0530

    edit working

--- screens/Edit.js
+++ screens/Edit.js
@@ -1,11 +1,90 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {Text, ScrollView, StyleSheet} from 'react-native';
+import {Container, Form, Item, Input, Button, H1} from 'native-base';
+import AsyncStorage from '@react-native-community/async-storage';
+import Snackbar from 'react-native-snackbar';
 
-export default function Edit() {
+export default function Edit({navigation, route}) {
+  const [name, setName] = useState('');
+  const [totalNumberOfSeason, setTotalNumberOfSeason] = useState('');
+  const [id, setId] = useState(null);
+  const [isWatched, setIsWatched] = useState('');
+
+  const updateSeries = async () => {
+    try {
+      // check if input is provided
+      if (!name || !totalNumberOfSeason) {
+        return Snackbar.show({
+          text: 'Please provide both fields',
+          duration: Snackbar.LENGTH_SHORT,
+          backgroundColor: '#00b7c2',
+          textColor: '#eee',
+        });
+      }
+      // make object to add in storage
+      const seriesToupdate = {
+        id,
+        name,
+        totalNumberOfSeason,
+        isWatched,
+      };
+      // get already created storage
+      const storedValues = await AsyncStorage.getItem('@series_list');
+      const prevList = await JSON.parse(storedValues);
+      prevList.map((item) => {
+        if (item.id == id) {
+          item.name = name;
+          item.totalNumberOfSeason = totalNumberOfSeason;
+          item.isWatched = isWatched;
+        }
+        return item;
+      });
+      // save to storage
+      await AsyncStorage.setItem('@series_list', JSON.stringify(prevList));
+
+      // navigate to home after adding
+      navigation.navigate('Home');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    const {item} = route.params;
+    const {id, name, totalNumberOfSeason, isWatched} = item;
+    setId(id);
+    setName(name);
+    setTotalNumberOfSeason(totalNumberOfSeason);
+    setIsWatched(isWatched);
+  }, []);
+
   return (
-    <View>
-      <Text>Edit Screen</Text>
-    </View>
+    <Container style={styles.container}>
+      <ScrollView contentContainerStyle={{flexGrow: 1}}>
+        <H1 style={styles.heading}>Edit Series</H1>
+        <Form>
+          <Item rounded style={styles.formItem}>
+            <Input
+              placeholder="Series Name"
+              style={{color: '#eee'}}
+              value={name}
+              onChangeText={(text) => setName(text)}
+            />
+          </Item>
+          <Item rounded style={styles.formItem}>
+            <Input
+              placeholder="Total number of seasons"
+              style={{color: '#eee'}}
+              value={totalNumberOfSeason}
+              onChangeText={(text) => setTotalNumberOfSeason(text)}
+            />
+          </Item>
+          <Button rounded block onPress={updateSeries}>
+            <Text style={{color: '#eee'}}>Save</Text>
+          </Button>
+        </Form>
+      </ScrollView>
+    </Container>
   );
 }
 
